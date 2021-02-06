b4cb04630188cf79fb7e251d30fb1f76b019cfaf
PrasannaMudumanchi <mudumanchiprasanna@gmail.com>
2021-02-06 10:00:32 +0530

    chaneges in checkout form

--- ecommerce_site/forms.py
+++ ecommerce_site/forms.py
@@ -16,7 +16,7 @@ class CheckoutForm(forms.Form):
         'class': 'custom-select d-block w-100'
     })
     zipCode = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
+    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput)
+    save_info = forms.BooleanField(widget=forms.CheckboxInput)
     payment_option = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
